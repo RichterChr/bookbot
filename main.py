50f9ad5c7b0952d389585d9f997d5228b8fd7624
RichterChr <richterchr@gmx.net>
2025-06-12 21:54:13 +0200

    cleaned up code

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from stats import word_count
-from stats import character_count
+from stats import word_count, character_count
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    num_words = word_count(file_contents)
+    chars_dict = character_count(file_contents)
+    print(f"{num_words} words found in the document")
+    print(chars_dict)
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
-
-def main():
-    file_contents = get_book_text("books/frankenstein.txt")
-    word_count(file_contents)
-    character_count(file_contents)
 
 
 main()
